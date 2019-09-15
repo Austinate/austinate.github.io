f81992f86777704d7d5547c8472428604be5ea4d
austinate <github@austinate.me>
2019-09-15 22:07:12 +0300

    Updates

--- src/utils/typography.js
+++
@@ -1,7 +0,0 @@
-import Typography from "typography"
-import twinPeaks from "typography-theme-twin-peaks"
-
-const typography = new Typography(twinPeaks)
-
-export const { scale, rhythm, options } = typography
-export default typography
