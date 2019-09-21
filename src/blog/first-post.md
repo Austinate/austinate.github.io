76af9089fc06bd66efd693ad0cca7dd2994e8482
austinate <github@austinate.me>
2019-09-21 14:55:58 +0300

    First post file added

---
+++ src/blog/first-post.md
@@ -0,0 +1,6 @@
+---
+title: "First post"
+date: "2019-09-21T12:00"
+---
+
+Here will be my first post
--- src/pages/index.js
+++ src/pages/index.js
@@ -44,7 +44,6 @@ export const query = graphql`
             slug
           }
           excerpt
-          html
         }
       }
     }
