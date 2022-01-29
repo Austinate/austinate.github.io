655549bdb05cdeafd1c51fe06751c3eb3a15f152
Ostap <me@austinate.me>
2022-01-29 15:44:49 +0200

    Automatic Gatsby deploy

---
+++ component---src-pages-index-js-7bec04ffe9621ef26271.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkaustinate_me=self.webpackChunkaustinate_me||[]).push([[678],{7704:function(e,t,n){n.r(t);var a=n(7294),l=n(2942);t.default=function(e){e.data;return a.createElement(l.Z,null,a.createElement("h1",{style:{textAlign:"center"}},"Blog in progress"," ",a.createElement("span",{role:"img","aria-label":"tools emoji"},"🛠")),a.createElement("p",{style:{textAlign:"center"}},'But you can always check the "About" section.'))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-7bec04ffe9621ef26271.js.map
